3c08d32610b203f295b5d9819884776cc43dcf08
Vinay288 <vinayhiremath288@gmail.com>
2021-09-18 01:26:57 +0530

    ADDED:add hotels method to getcheapest hotel price

--- lib/src/main/java/HotelManagement/HotelReservation.java
+++ lib/src/main/java/HotelManagement/HotelReservation.java
@@ -1,10 +1,13 @@
 package HotelManagement;
 
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class HotelReservation {
+	List<Hotel> hotels = new ArrayList<Hotel>();
 
 	public boolean addHotel(String hotelName, int rate) {
 		Scanner scanner = new Scanner(System.in);
@@ -12,5 +15,17 @@ public class HotelReservation {
 		Hotel hotel = new Hotel("Name", 2000);
 		hotels.add(hotel);
 		return true;
+	}
+
+	public Hotel getCheapestHotel(LocalDate startDate, LocalDate lastDate) {
+		long daysBetween = ChronoUnit.DAYS.between(startDate, lastDate);
+
+		Hotel cheapestHotel = hotels.stream()
+				.min((n1, n2) -> n1.getRateForRegularCustomer() - n2.getRateForRegularCustomer()).orElse(null);
+		long cheapestRate = (daysBetween + 1) * cheapestHotel.getRateForRegularCustomer();
+		System.out
+				.println("Cheapest hotel name is :" + cheapestHotel.getHotelName() + "Total rate is :" + cheapestRate);
+		return cheapestHotel;
+
 	}
 }
