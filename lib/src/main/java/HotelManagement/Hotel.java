9332b7ba65042a4d635fbb8433be1a8b943a901a
Vinay288 <vinayhiremath288@gmail.com>
2021-09-18 01:17:39 +0530

    ADDED class java with getters and setetrs method

--- lib/src/main/java/HotelManagement/Hotel.java
+++ lib/src/main/java/HotelManagement/Hotel.java
@@ -4,8 +4,32 @@
 package HotelManagement;
 
 public class Hotel {
-    public Hotel() {
-		
-	}
-    }
+	String hotelName;
+	int rateForRegularCustomer;
 
+	public Hotel(String hotelName, int rateForRegularCustomer) {
+		this.hotelName = hotelName;
+		this.rateForRegularCustomer = rateForRegularCustomer;
+	}
+
+	public String getHotelName() {
+		return hotelName;
+	}
+
+	public int getRateForRegularCustomer() {
+		return rateForRegularCustomer;
+	}
+
+	public void setHotelName(String hotelName) {
+		this.hotelName = hotelName;
+	}
+
+	public void setRateForRegularCustomer(int rateForRegularCustomer) {
+		this.rateForRegularCustomer = rateForRegularCustomer;
+	}
+
+	@Override
+	public String toString() {
+		return "hotelName=" + hotelName + ", rateForRegularCustomer=" + rateForRegularCustomer + "]";
+	}
+}
