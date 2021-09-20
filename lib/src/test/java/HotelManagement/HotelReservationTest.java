2789825df2a2f8790aef0cb36051a0ebf5e71f56
Vinay288 <vinayhiremath288@gmail.com>
2021-09-20 15:31:46 +0530

    MODIFIED: added ratings for the test to find best rated cheapest hotels

--- lib/src/test/java/HotelManagement/HotelReservationTest.java
+++ lib/src/test/java/HotelManagement/HotelReservationTest.java
@@ -28,9 +28,9 @@ public class HotelReservationTest {
 	@Test
 	public void givenOneHotel_WhenCorrect_ShoulReturnProperHotelName() {
 		HotelReservation listOfHotels = new HotelReservation();
-		Hotel hotel1 = new Hotel("Lakewood", 100, 200, 1);
-		Hotel hotel2 = new Hotel("Bridgewood", 150, 150, 2);
-		Hotel hotel3 = new Hotel("Ridgewood", 200, 300, 3);
+		Hotel hotel1 = new Hotel("Lakewood", 110, 90, 3);
+		Hotel hotel2 = new Hotel("Bridgewood", 150, 50, 4);
+		Hotel hotel3 = new Hotel("Ridgewood", 220, 150, 5);
 		listOfHotels.addHotel(hotel1);
 		listOfHotels.addHotel(hotel2);
 		listOfHotels.addHotel(hotel3);
@@ -38,7 +38,7 @@ public class HotelReservationTest {
 		LocalDate date2 = LocalDate.of(2020, Month.SEPTEMBER, 12);
 
 		Hotel cheapestHotel = listOfHotels.findCheapestHotel(date1, date2);
-System.out.println(cheapestHotel);
-		Assert.assertEquals("Lakewood", cheapestHotel.getHotelName());
+		System.out.println(cheapestHotel);
+		Assert.assertEquals("Bridgewood", cheapestHotel.getHotelName());
 	}
 }
