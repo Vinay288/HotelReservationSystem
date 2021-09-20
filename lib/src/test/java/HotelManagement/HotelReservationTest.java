3a66562aaa9a0d4a53d38d283aadd178ca4ab394
Vinay288 <vinayhiremath288@gmail.com>
2021-09-20 14:18:30 +0530

    MODIFIED: added weekday and weekend rates to test for cheapest hotel

--- lib/src/test/java/HotelManagement/HotelReservationTest.java
+++ lib/src/test/java/HotelManagement/HotelReservationTest.java
@@ -4,11 +4,13 @@
 package HotelManagement;
 
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Before;
@@ -26,15 +28,17 @@ public class HotelReservationTest {
 	@Test
 	public void givenOneHotel_WhenCorrect_ShoulReturnProperHotelName() {
 		HotelReservation listOfHotels = new HotelReservation();
-		Hotel hotel1 = new Hotel("Lakewood", 100, 0, 1);
-		Hotel hotel2 = new Hotel("Bridgewood", 150, 0, 2);
-		Hotel hotel3 = new Hotel("Ridgewood", 200, 0, 3);
+		Hotel hotel1 = new Hotel("Lakewood", 100, 200, 1);
+		Hotel hotel2 = new Hotel("Bridgewood", 150, 150, 2);
+		Hotel hotel3 = new Hotel("Ridgewood", 200, 300, 3);
 		listOfHotels.addHotel(hotel1);
 		listOfHotels.addHotel(hotel2);
 		listOfHotels.addHotel(hotel3);
-		LocalDateTime startDate = LocalDateTime.now();
-		LocalDateTime endDate = startDate.plusDays(2);
-		Hotel cheapest = listOfHotels.findCheapestHotel(startDate, endDate);
-		assertEquals("Lakewood", cheapest.getHotelName());
+		LocalDate date1 = LocalDate.of(2020, Month.SEPTEMBER, 11);
+		LocalDate date2 = LocalDate.of(2020, Month.SEPTEMBER, 12);
+
+		Hotel cheapestHotel = listOfHotels.findCheapestHotel(date1, date2);
+System.out.println(cheapestHotel);
+		Assert.assertEquals("Lakewood", cheapestHotel.getHotelName());
 	}
 }
