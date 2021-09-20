22225401548169770bf168ccb479080592ad1e8b
Vinay288 <vinayhiremath288@gmail.com>
2021-09-20 16:57:24 +0530

    ADDED: hotel reservation exception class with invalid and null enum types

---
+++ lib/src/main/java/HotelManagement/HotelReservationException.java
@@ -0,0 +1,15 @@
+package HotelManagement;
+
+public class HotelReservationException extends RuntimeException {
+
+	enum ExceptionType {
+		DATE_NULL, DATE_INVALID, CUSTOMER_TYPE_NULL, CUTOMER_TYPE_INVALID
+	}
+
+	ExceptionType type;
+
+	public HotelReservationException(String message, ExceptionType type) {
+		super(message);
+		this.type = type;
+	}
+}
